b962c2625a30abbb22bd5565966a0325a11d1855
maha <mmahaece07@gmail.com>
2021-12-31 12:16:43 +0530

    Added

---
+++ WinFormsApp1/WinFormsApp1/BankAccountForm1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp1.Models;
+
+namespace WinFormsApp1
+{
+    public partial class BankAccountForm1 : Form
+    {
+        public BankAccountForm1()
+        {
+            InitializeComponent();
+        }
+        private void BTnCreateObject_click(object sender, EventArgs e)
+        {
+            BankAccount bankAccount = new BankAccount();
+            
+
+           // BankAccount bankAccount1 = new BankAccount();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BankAccount bankAccount = new BankAccount();
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Models/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Models
+{
+   public class BankAccount
+    {
+        public int id;
+        public string Name;
+        public decimal Balance;
+
+        
+    }
+    public BankAccount()
+    {
+        MessageBox.Show("Bank account object Created");
+    }
+
+
+
+}
